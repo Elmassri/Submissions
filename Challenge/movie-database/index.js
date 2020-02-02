2b75f99b8298e0b4da579040947db2b5e75cc089
elmassri <mm.massri80@gmail.com>
2020-02-02 18:11:55 +0200

    step2 movieDataBase

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const port = 3000;
-const app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.get('/', (req, res) => res.send('Hello World!'))
 
-
-app.listen(3000, () => { `listening to port ${port}` });
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
